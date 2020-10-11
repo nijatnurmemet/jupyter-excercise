324313fc47b1d8fc541f33d3f0208bd8e0ee3e2e
nijatnurmemet <46684115+nijatnurmemet@users.noreply.github.com>
2020-10-11 23:15:24 +0200

    new file

---
+++ Atom.py
@@ -0,0 +1,24 @@
+a = "cmdv\
+dlmvldv lvddvd\
+d;v;,dv,d"
+print(a)
+flavour = "Apple Pie"
+print(flavour[1])
+print("bainga"[-1])
+print(len("bazinga"))
+print("bazinga"[6])
+print("bazinga"[2:-1])
+print("bazinga"[:3])
+print("bazinga"[-5:])
+string_with_space = "  nijat  nurmemet    "
+print(string_with_space)
+print(string_with_space.lstrip())
+print(string_with_space.startswith("n"))
+captain = "James T. Kirk"
+officer = "Spock"
+doc = "Bones"
+navigator = "Pavel Andreievich Chekov"
+print(captain,officer,doc,navigator)
+print("AAA".find("a"))
+print("Version 2.0".find("2.0"))
+print("Version 2.0"[8:])
